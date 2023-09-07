9b3d94cb36bc756691bbd9de7a69795c45b2371e
Karnisha Karunakaran <karnisha.k@itp.objectfrontier.com>
2023-09-07 12:40:47 +0530

    Bank

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+abstract class Bank{
+    public abstract void BankDetails();
+    public abstract void Deposit();
+    public abstract void Withdrawl();
+}
