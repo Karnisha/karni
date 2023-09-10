69e0c7c491ac30cb3a1434c7ea3cc022a609e4d6
Karnisha Karunakaran <karnisha.k@itp.objectfrontier.com>
2023-09-10 21:13:48 +0530

    hospital details

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+abstract class Hospital{
+    public abstract void HospitalDetails();
+    public abstract void PatientDetails();
+}
