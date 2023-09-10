9ef8371212aaf3dcaa4ecd6afac858eb41f57a87
Karnisha Karunakaran <karnisha.k@itp.objectfrontier.com>
2023-09-10 21:23:35 +0530

    implementation

--- Program.cs
+++ Program.cs
@@ -3,3 +3,26 @@ abstract class Hospital{
     public abstract void HospitalDetails();
     public abstract void PatientDetails();
 }
+class Details : Hospital{
+    public override void HospitalDetails()
+    {
+        Console.WriteLine("Hospital name:kk hospital");
+        Console.WriteLine("hospital location:thanjavur");
+    }
+
+  
+
+    public override void PatientDetails(){
+        Console.WriteLine("patient name:Abirami");
+        Console.WriteLine("patient contactno:1234567891");
+    }
+}
+
+  
+class Implementation{
+    public static void Main(string[] args){
+        Details Ref=new Details();
+        Ref.HospitalDetails();
+        Ref.PatientDetails();
+    }
+}
